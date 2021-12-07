99496edda3727cc9d3daaf67341e3999e48910dc
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2021-12-07 16:49:05 -0600

    part 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Fave Photos!';
+  image1 = 'https://picsum.photos/536/354';
+  image2 = 'https://picsum.photos/536/355';
+  image3 = 'https://picsum.photos/536/356';
 
   constructor() { }
 
